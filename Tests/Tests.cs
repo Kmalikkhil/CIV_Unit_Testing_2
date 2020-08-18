8e4c262a839d5d53d2377a737f3210ccf34ed0e6
Kanishka Malikkhil <103220762@student.swin.edu.au>
2020-08-19 01:32:40 +1000

    Task Completed

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -38,5 +38,56 @@ namespace Tests
             int Output = g1.IsFactiorialNum(num);
             Assert.Equal(expected, Output);
         }
+
+
+
+        //3.  Coverts a temperature from Celcius to Farenheit, rounded up to the nearest whole number
+
+        [Theory]
+        [InlineData(0,32)]
+        [InlineData(10,50)]
+
+        public void CelciusToFarenheit_Test(int Farenheit, int Celsius)
+        {
+            int Output = g1.CelciusToFarenheit(Celsius);
+            Assert.Equal(Farenheit, Output);
+        }
+
+
+        //4.  Checks a string and returns true if it is palindromic
+
+        [Theory]
+        [InlineData(true,"Anna")]
+        [InlineData(false,"rob")]
+
+        public void IsPalindrome_Test(bool expected, string word)
+        {
+            Assert.Equal(expected, g1.IsPalindrome(word));
+        }
+
+
+        //5.  Replaces a character when found in a string
+
+        [Theory]
+        [InlineData("Love Programming" , "Live Prigramming" , "i" , "o")]
+        [InlineData("cat" , "cot" , "o" , "a")]
+
+        public void StringReplace_Test(string expected, string word, char value1, char value2)
+        {
+            Assert.Equal(expected, g1.StringReplace(word,value1,value2));
+        }
+
+
+        //6.  Counts the number of times a character occurs in a string
+
+        [Theory]
+        [InlineData(2,"o","Book")]
+
+        public void CharCount_Test(int expected, string word, char countCharacter)
+        {
+            Assert.Equal(expected, g1.CharCount(word,countCharacter));
+        }
+        
+
     }
 }
